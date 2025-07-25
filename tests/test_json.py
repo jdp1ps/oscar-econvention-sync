323731c86af7232c2c6ae81e95ff9c486b149723
Lucas Zheng <lucas.zheng42@gmail.com>
2025-07-25 11:47:30 +0200

    feat: add some json test and black connect, fix: psycopg2 to psycopg2-binary

---
+++ tests/test_json.py
@@ -0,0 +1,28 @@
+import json
+import os
+
+def test_json_reading(path='demo_data/test_data_oscar.json'):
+    with open(path, 'r') as f:
+        data = json.load(f)
+    assert isinstance(data, list)
+    return data
+
+
+def test_json_writing(input_path='demo_data/test_data_oscar.json', output_path='demo_data/test_output.json'):
+    data = test_json_reading(input_path)
+
+    with open(output_path, 'w') as f:
+        json.dump(data, f, indent=4)
+
+    with open(output_path, 'r') as f:
+        data_written = json.load(f)
+
+    assert data == data_written
+
+#def test_json_convert_eConvention_To_Oscar(input_path='demo_data/test_data_econvention.json', output_path='demo_data/test_data_oscar.json'):
+
+if __name__ == '__main__':
+    test_json_reading()
+    test_json_writing()
+
+
