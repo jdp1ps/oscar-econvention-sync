4d262a45030ab8003c0b2d33b6e1969d3d129f5c
Lucas Zheng <lucas.zheng42@gmail.com>
2025-08-05 17:33:33 +0200

    fix(tests): attempt to fix global test failures in GitHub due to pylint issues

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,13 +3,20 @@ import sys
 import json
 from pathlib import Path
 import pytest
+from _pytest.fixtures import SubRequest
 import pendulum
 from airflow.models import DagBag
+from airflow import settings, DAG
+from airflow.utils.db import resetdb
+from tests.utils.dag import TEST_DAG_ID, DATA_INTERVAL_START
+from tests.utils.dag import import_from_path
+
+
+os.environ["APP_ENV"] = "TEST"
 
 dags_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "dags"))
 if dags_path not in sys.path:
     sys.path.insert(0, dags_path)
-
 
 @pytest.fixture(name="unique_logical_date")
 def unique_logical_date() -> pendulum.DateTime:
@@ -50,3 +57,54 @@ def oscar_expected_data():
     """
     with open(DATA_DIR / "oscar_expected_data.json", encoding="utf-8") as f:
         return json.load(f)
+
+@pytest.fixture(scope='function', autouse=True, name="initialize_airflow_db")
+def initialize_airflow_db() -> None:
+    """
+    Initialize the airflow database for testing and reset the database before each test
+    :return: None
+    """
+    os.environ['AIRFLOW__CORE__SQL_ALCHEMY_CONN'] = "sqlite:////:memory:"
+    resetdb()
+    settings.configure_orm()
+
+
+@pytest.fixture(name="dagbag")
+def fixture_dagbag() -> DagBag:
+    """
+    Create a DagBag for testing dag loading
+    :return: DagBag
+    """
+    airflow_home = os.getenv('AIRFLOW_HOME')
+    assert airflow_home is not None, "AIRFLOW_HOME is not set"
+    dag_folder = f"{airflow_home}/dags"
+    print(f"Dag folder is : {dag_folder}")
+    return DagBag(dag_folder=dag_folder)
+
+
+@pytest.fixture(name="dag")
+def dag_fixture(request: SubRequest) -> DAG:
+    """
+    Create a DAG for testing the convert_ldap_structure_description_task
+    :param request: The pytest request object
+    It is a dictionary with the following keys
+    - task_name: The name of the task to be created
+    - param_names: The names of the parameters to be passed to the task
+    - other keys: The values of the parameters to be passed to the task
+    :return: The DAG object
+    """
+    task_name = request.param['task_name']
+    param_names = request.param['param_names']
+    assert isinstance(param_names, list)
+    assert all(isinstance(name, str) for name in param_names)
+    args = [request.param[name] for name in param_names]
+
+    # pylint: disable=unexpected-keyword-arg
+    with DAG(
+            dag_id=TEST_DAG_ID,
+            schedule="@daily",
+            start_date=DATA_INTERVAL_START,
+    ) as created_dag:
+        task = import_from_path(task_name)
+        task(*args)
+    return created_dag
--- tests/utils/dag.py
+++ tests/utils/dag.py
@@ -68,6 +68,17 @@ def create_task_instance(dag: DAG, dag_run: DagRun, task_id: str) -> TaskInstanc
     ti.run(ignore_ti_state=True)
     return ti
 
+def import_from_path(path: str) -> callable:
+    """
+    Import an Airflow task from a module
+    :param path: dot separated path to the task,
+    e.g. tasks.supann_2021.convert_ldap_structure_name.convert_ldap_structure_name_task
+    :return: The task function
+    """
+    module_name, function_name = path.rsplit('.', 1)
+    print(f"Importing task {function_name} from module {module_name}")
+    module = __import__(module_name, fromlist=[function_name])
+    return getattr(module, function_name)
 
 # pylint: disable=unexpected-keyword-arg
 with DAG(
