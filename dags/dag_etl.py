09bcbc6c440aa5575fe99ea9a04a676eb0f9f8da
Lucas Zheng <lucas.zheng42@gmail.com>
2025-08-07 11:39:19 +0200

    chore: move the task "extract_from_econvention" from dags/dag_etl.py to dags/tasks/extract.py

--- dags/dag_etl.py
+++ dags/dag_etl.py
@@ -1,32 +1,10 @@
 from datetime import datetime
 from airflow import DAG
 from airflow.providers.standard.operators.bash import BashOperator
-from airflow.decorators import task
+from tasks.extract import extract_from_econvention
 from tasks.transform import transform_from_econvention_to_oscar
 from tasks.load import load
 from utils.config import OSCAR_HOME_PATH
-
-
-@task
-def extract_from_econvention(**context) -> list[dict]:
-    """
-    Extract data from REST API POST sent by eConvention
-
-    :param context: given automatically by Airflow
-
-    :return:
-        list of dictionaries
-    """
-    conf_json = context["dag_run"].conf
-    items = conf_json.get("items")
-    if items is None:
-        raise ValueError("Missing 'items' key in dag_run.conf.")
-    if not isinstance(items, list):
-        raise ValueError("Expected 'items' to be a list of dictionaries.")
-    if not all(isinstance(item, dict) for item in items):
-        raise ValueError("All items in 'items' must be dictionaries.")
-
-    return items
 
 
 # pylint: disable=unexpected-keyword-arg
