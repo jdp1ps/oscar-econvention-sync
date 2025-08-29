2abeac450b0b6a1e3cf15e9926ddaebb7d4e53b5
Lucas Zheng <lucas.zheng42@gmail.com>
2025-08-29 16:50:07 +0200

    chore: move tasks in oscar_to_econvention.py fix: remove f-string SQL request to prevent SQL injection

--- dags/oscar_to_econvention.py
+++ dags/oscar_to_econvention.py
@@ -1,40 +1,11 @@
 from datetime import datetime
 from airflow import DAG
-from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.decorators import task
-from utils.config import POSTGRES_CONN_ID, BASCULE_ECONVENTION_ID
+from tasks.oscar_to_econvention.pg_get_activity_types import pg_extract_activity_types
+from tasks.oscar_to_econvention.pg_get_migratable_activities import (
+    pg_get_migratable_activities,
+)
 
 ACTIVITY_TYPE_LIST = None
-
-
-@task
-def pg_extract_activity_types():
-    """[TEMPORARY] Extract activity types from Postgres table"""
-    activity_types = []
-    hook = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
-    conn = hook.get_conn()
-    cursor = conn.cursor()
-    cursor.execute("SELECT id,label FROM ACTIVITYTYPE;")
-    for row in cursor.fetchall():
-        activity_types.append(row)
-    return activity_types
-
-
-@task
-def pg_extract_activities():
-    """[TEMPORARY] Extract activities from Postgres table"""
-    basculed_activities = []
-    hook = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
-    conn = hook.get_conn()
-    cursor = conn.cursor()
-    cursor.execute(
-        "select * from activity,activitydate "
-        "where activitydate.activity_id = activity.id "
-        f"and activitydate.type_id = {BASCULE_ECONVENTION_ID};"
-    )
-    for row in cursor.fetchall():
-        basculed_activities.append(row)
-    return basculed_activities
 
 
 # pylint: disable=unexpected-keyword-arg
@@ -46,4 +17,4 @@ with DAG(
     tags=["api", "json", "etl", "post"],
 ) as oscar_to_econvention:
     activity_type_list = pg_extract_activity_types()
-    activities = pg_extract_activities()
+    activities = pg_get_migratable_activities()
